0682603f49f13efd62143b83e7fa73eced0a5e34
hariharaprabhu <hariharaprabhu.narayanan@gmail.com>
2015-07-25 17:27:26 -0400

    update cachematrix.R
    The contents of the function has been updated according to the requirements. The comments has been updated at the begining of the each function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,88 @@
-## Put comments here that give an overall description of what your
-## functions do
+#makeCacheMatrix function contains 4 sub functions set,get,setsolve,getsolve
 
-## Write a short comment describing this function
+#the set function will take the value of the input (say x here) and store it in a matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+#the i is the name of the matrix where we are going to store the inverse of the input
 
-}
+#the value of i is initially set to null so that if there exists a pre value of i it will not be create confusion over here
+
+#now the value of the matrix is set, the get function is used to return the matrix stored in the set function
+
+#the setsolve function is use to compute the inverse of the current matrix, the solve command is used to find the inverse
+
+#note for finding the inverse of something the matrix should be a square matrix
+
+#the get solve function is used to return the value stored in the matrix i
+
+#the list is used to store the 4 functions 
 
 
-## Write a short comment describing this function
+makeCacheMatrix <- function(x = matrix())
+
+  {
+      
+    i <- NULL
+    
+    set <- function(y) 
+      
+      {
+        
+        x <<- y
+        
+        i <<- NULL
+      }
+  
+     get <- function() x
+     
+     setsolve <- function(solve) i <<- solve
+   
+     getsolve <- function() i
+  
+     list(set = set, get = get,
+          
+          setsolve = setsolve,
+          
+          getsolve = getsolve)
+  
+  
+  }
+
+#the cache solve function contains 3 functions getsolve, get and setsolve
+
+#this function is used to check if a matrix inverse has already been computed 
+
+#the getsolve() will check if a matrix inverse i is already available, 
+# if so it displays it current or the cached value of the matrix i
+
+#if the matrix inverse is not determined before then the get function will return the value stored in x
+
+#the solve function will compute the inverse of the matrix
+
+#the setsolve function will store the inverse of the matrix value into i
+
+#finally the matrix i which is the inverse of the input x is returned
+
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+                                  
+                                  i <- x$getsolve()
+                                  
+                                  if(!is.null(i)) {
+                                                    
+                                                    message("getting cached data")
+                                    
+                                                    return(i)
+                                                   
+                                                    }
+                                  
+                                  data <- x$get()
+                                  
+                                  i <- solve(data, ...)
+                                  
+                                  x$setsolve(i)
+                                  
+                                  i
+                                
+                                  }
+
